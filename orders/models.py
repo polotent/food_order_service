0e0056b28e360279b8dcbaf8919375f2e1bf38d9
polotent <alexmirosh37@gmail.com>
2021-08-31 01:35:09 +0300

    updated models

--- orders/models.py
+++ orders/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-
-class MenuItem(models.Model):
-    pass
-
 class Menu(models.Model):
     menu_name = models.CharField(max_length=200)
 
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    item_price = models.IntegerField(default=0)
 
 class Restaurant(models.Model):
     restaurant_name = models.CharField(max_length=200)
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-
+    menu = models.ForeignKey(Menu, on_delete=models.DO_NOTHING)
 
 class Order(models.Model):
-    pass
+    order_date = models.DateTimeField()
+    order_restaurant = models.ForeignKey(Restaurant, on_delete=models.DO_NOTHING)
+    order_user_addr = models.CharField(max_length=200)
+    order_user_phone = models.CharField(max_length=200)
+    order_status = models.CharField(max_length=200)
 
 class OrderItem(models.Model):
-    pass
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    menu_item = models.ForeignKey(Item, on_delete=models.DO_NOTHING)
+    order_item_price = models.IntegerField(default=0)
+    order_item_quantity = models.IntegerField(default=0)
