93f47b6034eb247fedec3392adedf25d9c4c42a1
polotent <alexmirosh37@gmail.com>
2021-09-20 02:16:51 +0300

    added working logging

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,22 @@
 import json
 import logging
+import os
 from datetime import datetime
 from django.http import JsonResponse
 from django.db import DatabaseError
 from .models import Item, Restaurant, Menu, Order, OrderItem
 from .helpers import calc_total_price
 
-logger = logging.getLogger(__name__)
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+logs_path = os.path.join(BASE_DIR, 'logs')
+if not os.path.isdir(logs_path):
+    os.mkdir(logs_path)
+
+logging.basicConfig(
+    filename=os.path.join(logs_path, 'ordersapp.log'),
+    filemode='w',
+    level=logging.INFO
+)
 
 
 def restaurants(request):
@@ -17,7 +27,7 @@ def restaurants(request):
 
     if request.method != 'POST':
         response = {'error': 'Request method must be POST'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
@@ -26,8 +36,8 @@ def restaurants(request):
         )
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(f'\"{request.path}\" Error during db transaction: {e}')
-    logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Error during db transaction: {e}')
+    logging.info(f'\"{request.path}\" Sending response: {response}')
     return JsonResponse(response)
 
 
@@ -40,39 +50,39 @@ def menu(request):
 
     if request.method != 'POST':
         response = {'error': 'Request method must be POST'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
         request_data = json.loads(request.body)
     except json.decoder.JSONDecodeError:
         response = {'error': 'Wrong data format'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     menu_id = request_data.get('menu_id')
 
     if not menu_id:
         response = {'error': 'Missing id parameter for menu'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if type(menu_id) != int:
         response = {'error': 'Menu menu_id parameter must be int type'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
         menu_qs = Menu.objects.filter(pk=menu_id).first()
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(f'\"{request.path}\" Error during db transaction: {e}')
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Error during db transaction: {e}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if not menu_qs:
         response = {'error': 'Menu not found with requested id'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     response['menu_name'] = menu_qs.menu_name
@@ -87,12 +97,12 @@ def menu(request):
         )
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(f'\"{request.path}\" Error during db transaction: {e}')
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Error during db transaction: {e}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
 
     response['menu_items'] = menu_items
 
-    logger.info(f'\"{request.path}\" Sending response: {response}')
+    logging.info(f'\"{request.path}\" Sending response: {response}')
     return JsonResponse(response)
 
 
@@ -106,31 +116,31 @@ def preorder(request):
 
     if request.method != 'POST':
         response = {'error': 'Request method must be POST'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
         request_data = json.loads(request.body)
     except json.decoder.JSONDecodeError:
         response = {'error': 'Wrong data format'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     item_list = request_data.get('items')
     if not item_list:
         response = {'error': 'Item list is empty'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if type(item_list) != list:
         response = {'error': 'Items must be represented as a list'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     for item in item_list:
         if type(item) != dict:
             response = {'error': 'Wrong request format'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         item_id = item.get('id')
@@ -138,44 +148,44 @@ def preorder(request):
 
         if not item_id:
             response = {'error': 'Missing id parameter for item'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         if type(item_id) != int:
             response = {'error': 'Item id parameter must be int type'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         if not item_quantity and item_quantity != 0:
             response = {'error': 'Missing quantity parameter for item'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         if type(item_quantity) != int:
             response = {'error': 'Item quantity parameter must be int type'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         if item_quantity <= 0:
             response = {
                 'error': f'Item quantity parameter must be positive, {item_quantity} was given'
             }
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         try:
             item_qs = Item.objects.filter(pk=item_id).first()
         except DatabaseError as e:
             response = {'error': 'Error during db transaction'}
-            logger.info(
+            logging.info(
                 f'\"{request.path}\" Error during db transaction: {e}'
             )
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         if not item_qs:
             response = {'error': 'Item not found with requested id'}
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
         item['price'] = item_qs.item_price
@@ -193,10 +203,10 @@ def preorder(request):
         order.save()
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(
+        logging.info(
             f'\"{request.path}\" Error during db transaction: {e}'
         )
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     response['order_id'] = order.id
@@ -214,10 +224,10 @@ def preorder(request):
             order_item.save()
         except DatabaseError as e:
             response = {'error': 'Error during db transaction'}
-            logger.info(
+            logging.info(
                 f'\"{request.path}\" Error during db transaction: {e}'
             )
-            logger.info(f'\"{request.path}\" Sending response: {response}')
+            logging.info(f'\"{request.path}\" Sending response: {response}')
             return JsonResponse(response)
 
     return JsonResponse(response)
@@ -233,14 +243,14 @@ def order(request):
 
     if request.method != 'POST':
         response = {'error': 'Request method must be POST'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
         request_data = json.loads(request.body)
     except json.decoder.JSONDecodeError:
         response = {'error': 'Wrong data format'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     order_id = request_data.get('order_id')
@@ -249,47 +259,47 @@ def order(request):
 
     if not order_id:
         response = {'error': 'Missing id parameter for order'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if type(order_id) != int:
         response = {'error': 'Order id parameter must be int type'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if not user_addr:
         response = {'error': 'Missing user_addr parameter for order'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if type(user_addr) != str:
         response = {'error': 'Order user_addr parameter must be str type'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if not user_phone:
         response = {'error': 'Missing user_phone parameter for order'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if type(user_phone) != str:
         response = {'error': 'Order user_phone parameter must be str type'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     try:
         order_qs = Order.objects.filter(pk=order_id).first()
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(
+        logging.info(
             f'\"{request.path}\" Error during db transaction: {e}'
         )
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     if not order_qs:
         response = {'error': 'Order not found with requested id'}
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     order_qs.user_addr = user_addr
@@ -300,10 +310,10 @@ def order(request):
         order_qs.save()
     except DatabaseError as e:
         response = {'error': 'Error during db transaction'}
-        logger.info(
+        logging.info(
             f'\"{request.path}\" Error during db transaction: {e}'
         )
-        logger.info(f'\"{request.path}\" Sending response: {response}')
+        logging.info(f'\"{request.path}\" Sending response: {response}')
         return JsonResponse(response)
 
     response['status'] = 'accepted'
